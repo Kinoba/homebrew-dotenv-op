17c7e1ca5082890bc4550d5485066d7f56ecb4bf
Grégoire Willmann <5655900+GWillmann@users.noreply.github.com>
2022-02-16 14:43:40 +0100

    fix: Github Action linter warnings

--- Formula/dotenv-op.rb
+++ Formula/dotenv-op.rb
@@ -10,14 +10,14 @@ class DotenvOp < Formula
   end
 
   def post_install
-    return if ENV["CI"]
+    return if ENV["HOMEBREW_GITHUB_ACTIONS"]
 
     puts <<~EOS
 
       ###############################################################################################################
       #                                                                                                             #
       # Don't forget to add the following variables to your .bash_profile/.zshrc:                                   #
-      #                                                                                                             #    
+      #                                                                                                             #
       # export DOTENVOP_VAULT="[vault]"          => The .env file will be stored in this vault                      #
       # export DOTENVOP_ACCOUNT="[name]"         => This is your 1Password account name                             #
       # export DOTENVOP_EMAIL="[your@email.com]" => This is the email you use to connect to your 1Password account  #
