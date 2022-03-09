ae5c15cf20a05dc0fe9b3b371a187bccffb2ccec
Grégoire Willmann <5655900+GWillmann@users.noreply.github.com>
2022-03-09 13:39:20 +0100

    build: bump dotenv-op version to v0.3.0

--- Formula/dotenv-op.rb
+++ Formula/dotenv-op.rb
@@ -1,8 +1,8 @@
 class DotenvOp < Formula
   desc "Manage and share your dotenv files with 1Password from your terminal"
   homepage "https://github.com/Kinoba/dotenv-op"
-  url "https://github.com/Kinoba/dotenv-op/releases/download/v0.2.1/dotenv-op.tar.gz"
-  sha256 "ef608f5182a7d34d7a0a3196505112c6ba529e92d7a7fdafe3f7b7f089429894"
+  url "https://github.com/Kinoba/dotenv-op/releases/download/v0.3.0/dotenv-op.tar.gz"
+  sha256 "f63a7dddf4a11836d9ca387b221349daa2c8f92d76a180a345fe2bff247bde1b"
   license "MIT"
 
   def install
